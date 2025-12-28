61728800129269e096d9d2d87ceadac7026974fb
Roger Costa <163103963+oger2004@users.noreply.github.com>
2025-12-28 22:48:43 +0100

    lk

--- pages/partials/footer/footer.js
+++ pages/partials/footer/footer.js
@@ -1,4 +1,4 @@
-fetch("./pages/partials/footer/footer.html")
+fetch("pages/partials/footer/footer.html")
     .then(res => res.text())
     .then(html => {
     document.getElementById("footer-placeholder").innerHTML = html;
